e60b2b3a83bf23f10890747bc7d42c8029484475
acidweasel <zhaa09@mail.ru>
2022-07-16 23:23:26 +0300

    Update weather_widget.js

--- weather_widget.js
+++ weather_widget.js
@@ -1,3 +1,6 @@
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={5386a06507150486a1d50fbc96a8f7e8}
+<script>
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={5386a06507150486a1d50fbc96a8f7e8}
 
 alert("!!!");
+
+</script>
